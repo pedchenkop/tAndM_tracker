439c693a401f0b0fe9eb02a000ebd5ef25d99c09
Artem Pedchenko <sale@pampam.com.ua>
2014-05-13 20:51:39 +0200

    first_change test

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -62,7 +62,7 @@
 	<div class="clear"></div>
 
 	<div id="footer">
-		Copyright &copy; <?php echo date('Y'); ?> TandM Studio<br/>
+		Copyright &copy; <?php echo date('Y'); ?> byy TandM Studio<br/>
 		All Rights Reserved.<br/>
 		<?php echo Yii::powered(); ?>
 	</div><!-- footer -->
